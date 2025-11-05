2c2175d2bc7cb9e2b3bd7898e9ac48c79ea8157d
b0ses <b0ses@users.noreply.github.com>
2025-11-04 22:50:03 -0500

    Testing

--- roles/configure_gunicorn/templates/conf.py
+++ roles/configure_gunicorn/templates/conf.py
@@ -9,3 +9,5 @@ group = "{{ gid }}"
 umask = "775"
 # mode = "775"
 capture_output = True
+keyfile = "/etc/letsencrypt/live/{{ domain_name }}/privkey.pem;"
+certfile = "/etc/letsencrypt/live/{{ domain_name }}/fullchain.pem;"
